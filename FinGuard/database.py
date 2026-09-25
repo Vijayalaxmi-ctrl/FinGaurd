6326587b5d609b6d79a5eff0ccde00dc2de04e2d
Vijayalaxmi-ctrl <vijayalaxmi102004@gmail.com>
2026-09-25 17:25:56 +0530

    changes

--- FinGuard/database.py
+++ FinGuard/database.py
@@ -2,7 +2,13 @@ import sqlite3
 import os
 
 # Database location
-DB_PATH = os.path.join("data", "finguard.db")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_PATH = os.path.join(
+    BASE_DIR,
+    "data",
+    "finguard.db"
+)
 
 
 def get_connection():
